f4e3a76afc26ec39b53e377d0045b6c701b81a14
Lira-v1 <sakeev.ilyas@gmail.com>
2025-09-23 15:41:42 +0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ВАЖНО: base = '/AVC_PRO/' — имя твоего репозитория на GitHub.
-// Если переименуешь репозиторий, измени и это значение.
-export default defineConfig({
-  plugins: [react()],
-  base: '/AVC_PRO/'
-})
